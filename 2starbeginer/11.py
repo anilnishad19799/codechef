ae86473a7381523c7444b501598a8820c8ba779d
Anil <anilnishad19799@gmail.com>
2023-03-08 00:37:13 +0530

    olve https://www.codechef.com/problems/XYSTR problem

---
+++ 2starbeginer/11.py
@@ -0,0 +1,23 @@
+#  https://www.codechef.com/problems/XYSTR
+
+test_cases = int(input())
+
+for _ in range(test_cases):
+    string = input()
+    count=0
+    i = 0
+    while True:
+        if i >= len(string) - 1:
+            break
+        if string[i] == 'x' and string[i+1]=='y':
+            i+=2
+            count+=1
+        elif string[i] == 'y' and string[i+1]=='x':
+            i+=2
+            count+=1
+        else:
+            i+=1
+            pass
+
+        
+    print(count)
